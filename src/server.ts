55f6a84cda33895a98a354db171510a3454d51c5
Vertical_Innovations <noreply@vertical-innovations.com>
2025-04-06 17:03:20 +0600

    feat: Added Logger

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,24 @@
 import app from './app';
 import config from './config/config';
+import logger from './utils/logger';
 
 const server = app.listen(config.PORT);
 
 (() => {
   try {
     // Database connection (You can actually connect here if needed)
-    console.info('APPLICATION_STARTED', {
+    logger.info('APPLICATION_STARTED', {
       meta: {
         PORT: config.PORT,
         SERVER_URL: config.SERVER_URL
       }
     });
   } catch (err) {
-    console.error('APPLICATION_ERROR', { meta: err });
+    logger.error('APPLICATION_ERROR', { meta: err });
     
     server.close((error: Error | undefined) => {
       if (error) {
-        console.error('APPLICATION ERROR', { meta: error });
+        logger.error('APPLICATION ERROR', { meta: error });
       }
 
       process.exit(1);
