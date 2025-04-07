0b3c58a03a5dc67a0ceb3a634364a6008fefefb4
Vertical_Innovations <noreply@vertical-innovations.com>
2025-04-07 12:49:24 +0600

    feat: Setup MongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import app from './app';
 import config from './config/config';
+import databaseService from './service/databaseService';
 import logger from './utils/logger';
 
 const server = app.listen(config.PORT);
 
-(() => {
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async() => {
   try {
     // Database connection (You can actually connect here if needed)
+    const connection = await databaseService.connect()
+    logger.info('DATABASE_CONNECTION', {
+      meta: {
+        CONNECTION_NAME: connection.name,
+      }
+    });
+
     logger.info('APPLICATION_STARTED', {
       meta: {
         PORT: config.PORT,
---
+++ src/service/databaseService.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import config from '../config/config';
+
+export default {
+    connect: async() => {
+        // eslint-disable-next-line no-useless-catch
+        try{
+            await mongoose.connect(config.DATABASE_URL as string);
+            return mongoose.Connection
+
+        } catch (err){
+            throw err;
+
+        }
+
+    }
+}
