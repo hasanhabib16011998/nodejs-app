16c5cc2153b6654573733c1376c1ee5a752c38d4
hasanhabib16011998 <hasan16habib@gmail.com>
2025-04-08 07:45:01 +0600

    feat: Setup Healthcheck

--- src/controller/apiController.ts
+++ src/controller/apiController.ts
@@ -2,15 +2,26 @@ import { NextFunction, Request, Response } from 'express';
 import httpResponse from '../utils/httpResponse';
 import responseMessage from '../constant/responseMessage';
 import httpError from '../utils/httpError';
+import quicker from '../utils/quicker';
 
 export default {
     self: (req: Request, res: Response, next: NextFunction) => {
         try{
-            throw new Error('This is error')
             httpResponse(req, res, 200, responseMessage.SUCCESS)
         } catch(err) {
             httpError(next, err, req, 500)
         }
-
-    }
+    },
+    health: (req: Request, res: Response, next: NextFunction) => {
+        try{
+            const healthData = {
+                application: quicker.getApplicationHealth(),
+                system: quicker.getSystemHealth(),
+                timestamp: Date.now()
+            }
+            httpResponse(req, res, 200, responseMessage.SUCCESS, healthData)
+        } catch(err) {
+            httpError(next, err, req, 500)
+        }
+    },
 }
--- src/router/apiRouter.ts
+++ src/router/apiRouter.ts
@@ -4,4 +4,5 @@ import apiController from '../controller/apiController';
 const router = Router();
 
 router.route('/self').get(apiController.self)
+router.route('/health').get(apiController.health)
 export default router;
